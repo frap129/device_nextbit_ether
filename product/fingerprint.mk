1e63d94eb8138139aa608774730ee06bb323fdbe
LuK1337 <priv.luk@gmail.com>
2017-09-19 09:50:10 +0530

    ether: Add basic 2.0 -> 2.1 fingerprint HAL wrapper
     * Adapt wrapper to load fp module conditionally
    
    Signed-off-by: Joe Maples <joe@frap129.org>

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -1,6 +1,7 @@
 PRODUCT_PACKAGES += \
     fingerprintd \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.0-service \
+    fingerprint.msm8992.so
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
