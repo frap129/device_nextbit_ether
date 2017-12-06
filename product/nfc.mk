18cef5b45cf2e626cdbe765a7585dd53631c09d3
Joe Maples <joe@frap129.org>
2017-12-05 22:17:04 -0500

    ether: Use AOSP NFC HAL
    Signed-off-by: Joe Maples <joe@frap129.org>

--- product/nfc.mk
+++ product/nfc.mk
@@ -1,14 +1,15 @@
 # NFC packages
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
-    nfc_nci.pn54x.default \
+    nfc_nci.msm8992 \
     NfcNci \
-    Tag
+    Tag \
+    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
-    $(LOCAL_PATH)/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
